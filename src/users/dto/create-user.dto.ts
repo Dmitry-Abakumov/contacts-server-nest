6b42e9f56e81188136d4eff318cacebb0ef68fcd
Dmitry Abakumov <dm.abakumov@gmail.com>
2026-06-28 11:36:50 +0300

    fix: ts error

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,10 +3,10 @@ import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
-  email: string;
+  email!: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(6, { message: 'Password should contain at least 6 characters' })
-  password: string;
+  password!: string;
 }
